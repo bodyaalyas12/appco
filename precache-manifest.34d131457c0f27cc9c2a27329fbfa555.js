e6375e52d450641f1000d290ac623877390ae3ca
bohdan <bodyaalyas12@gmail.com>
2020-02-22 00:30:54 +0200

    Updates

---
+++ precache-manifest.34d131457c0f27cc9c2a27329fbfa555.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2794b554db86c9e0d0d9f743d7ce6864",
+    "url": "/appco/index.html"
+  },
+  {
+    "revision": "803d6c3a91ffa8d01220",
+    "url": "/appco/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "89b8b27cba0f261751aa",
+    "url": "/appco/static/css/main.ad5cd113.chunk.css"
+  },
+  {
+    "revision": "803d6c3a91ffa8d01220",
+    "url": "/appco/static/js/2.59a7a09e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/appco/static/js/2.59a7a09e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89b8b27cba0f261751aa",
+    "url": "/appco/static/js/main.50a8129c.chunk.js"
+  },
+  {
+    "revision": "6e4468f50cac086235aa",
+    "url": "/appco/static/js/runtime-main.fb07a892.js"
+  },
+  {
+    "revision": "601fa87d30e61766a74b46c77b533dab",
+    "url": "/appco/static/media/Rectangle-51.601fa87d.svg"
+  },
+  {
+    "revision": "15da2b50d4737b49219df19b43e4e0bc",
+    "url": "/appco/static/media/Rectangle-54.15da2b50.svg"
+  },
+  {
+    "revision": "46cc3e2cf452fe1f977f39dbb21b74bf",
+    "url": "/appco/static/media/Rectangle-8.46cc3e2c.svg"
+  },
+  {
+    "revision": "596255f15cda70f6178a6a1b328e455c",
+    "url": "/appco/static/media/creativeThinking.596255f1.png"
+  },
+  {
+    "revision": "c51025171d97bcf63b9fdd489cad8f68",
+    "url": "/appco/static/media/macbook-min.c5102517.png"
+  },
+  {
+    "revision": "941eb1ba707af17fef14e61613695532",
+    "url": "/appco/static/media/macbook.941eb1ba.png"
+  },
+  {
+    "revision": "e83beaf3eccdb6900832ba3f099db8fc",
+    "url": "/appco/static/media/mobile-min.e83beaf3.png"
+  },
+  {
+    "revision": "e1e8301c0ca03cf7241c65f4d14a8493",
+    "url": "/appco/static/media/secure.e1e8301c.png"
+  }
+]);
