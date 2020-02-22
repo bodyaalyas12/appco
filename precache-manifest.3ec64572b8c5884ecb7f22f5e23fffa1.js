4158bc963b3b33a9e79fc9e7ef5ef2fe9256f078
bohdan <bodyaalyas12@gmail.com>
2020-02-23 00:53:29 +0200

    Updates

---
+++ precache-manifest.3ec64572b8c5884ecb7f22f5e23fffa1.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ba052245ae7425813cf7de2eef8fc23",
+    "url": "/appco/index.html"
+  },
+  {
+    "revision": "a9a98abe525b2187dcde",
+    "url": "/appco/static/css/2.389269c8.chunk.css"
+  },
+  {
+    "revision": "dee23ecaa62d93f3ef32",
+    "url": "/appco/static/css/main.8aa45cf7.chunk.css"
+  },
+  {
+    "revision": "a9a98abe525b2187dcde",
+    "url": "/appco/static/js/2.1f28bf6d.chunk.js"
+  },
+  {
+    "revision": "026703945c7c6cdacb86be11595cbba0",
+    "url": "/appco/static/js/2.1f28bf6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dee23ecaa62d93f3ef32",
+    "url": "/appco/static/js/main.bba1fe45.chunk.js"
+  },
+  {
+    "revision": "6e4468f50cac086235aa",
+    "url": "/appco/static/js/runtime-main.fb07a892.js"
+  },
+  {
+    "revision": "601fa87d30e61766a74b46c77b533dab",
+    "url": "/appco/static/media/Rectangle-51.601fa87d.svg"
+  },
+  {
+    "revision": "15da2b50d4737b49219df19b43e4e0bc",
+    "url": "/appco/static/media/Rectangle-54.15da2b50.svg"
+  },
+  {
+    "revision": "46cc3e2cf452fe1f977f39dbb21b74bf",
+    "url": "/appco/static/media/Rectangle-8.46cc3e2c.svg"
+  },
+  {
+    "revision": "596255f15cda70f6178a6a1b328e455c",
+    "url": "/appco/static/media/creativeThinking.596255f1.png"
+  },
+  {
+    "revision": "440d6ab028bd3bacae839fa1ede73b12",
+    "url": "/appco/static/media/keyboard_arrow_right-24px.440d6ab0.svg"
+  },
+  {
+    "revision": "c51025171d97bcf63b9fdd489cad8f68",
+    "url": "/appco/static/media/macbook-min.c5102517.png"
+  },
+  {
+    "revision": "941eb1ba707af17fef14e61613695532",
+    "url": "/appco/static/media/macbook.941eb1ba.png"
+  },
+  {
+    "revision": "e83beaf3eccdb6900832ba3f099db8fc",
+    "url": "/appco/static/media/mobile-min.e83beaf3.png"
+  },
+  {
+    "revision": "e1e8301c0ca03cf7241c65f4d14a8493",
+    "url": "/appco/static/media/secure.e1e8301c.png"
+  }
+]);
